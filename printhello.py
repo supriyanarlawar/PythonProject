19a3e2704a9326657e14973953a1f181ad5980ba
supriyakosalge <nandini19sep@gmail.com>
2013-11-19 16:06:49 -0500

    comit by sup

--- printhello.py
+++ printhello.py
@@ -3,3 +3,15 @@ __author__ = 'gazelle'
 print 'Supriya Kosalge'
 print 'supriya' + ' atish'
 print '300 \t terrace avenue\n jersey city nj'
+a = 1
+b = 2
+print a + b
+print a * b
+truebool = True
+c = False
+print truebool
+print c
+
+
+
+
